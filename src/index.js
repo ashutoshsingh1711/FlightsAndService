97dabd7890945dd296c64b827f0fe3d93045c1b0
Ashutosh Singh <ashutosh.singh32@infosys.com>
2023-02-11 15:07:23 +0530

    setup basic .env

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
- 
+const express = require('express');
+const dotenv =   require('dotenv').config()
+const {PORT} = require('./config/serverConfig')
+const setupAndStartServer = async () => {
+
+    //create express object
+    const app = express();
+    app.listen(PORT, () => { 
+        console.log(`Server started at ${PORT}`);
+    })
+ }  
+ setupAndStartServer(); 
